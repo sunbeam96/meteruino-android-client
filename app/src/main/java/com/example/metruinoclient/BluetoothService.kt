2c6e9c603cb8b8bc630fe911806395867b1be4ef
sunbeam96 <qba.lukaszczyk@gmail.com>
2023-06-30 23:11:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/metruinoclient/BluetoothService.kt
@@ -0,0 +1,11 @@
+package com.example.metruinoclient
+
+import android.view.Menu
+
+class BluetoothService {
+}
+
+override fun onConnectClick(): Boolean {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    return true
+}
